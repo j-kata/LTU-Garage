699d723cff47fb08ead2f26058544269683a8db9
katja tsareva <katja.tcareva@gmail.com>
2025-06-17 23:23:15 +0200

    add util functions

--- GarageApp/Helpers/Util.cs
+++ GarageApp/Helpers/Util.cs
@@ -34,4 +34,23 @@ public static class Util
 
         return int.Parse(validString);
     }
+
+    // TODO: Remove duplicate function. Rewrite as one generic function for both int and double
+    public static double PromptUntilValidDouble(IUI ui, string prompt, Func<double, bool> predicate = null)
+    {
+        string validString = PromptUntilValidString(ui, prompt, (validString) =>
+            double.TryParse(validString, out double number) &&
+            (predicate is null || predicate(number)));
+
+        return int.Parse(validString);
+    }
+
+    public static TEnum PromptUnilValidEnumChoice<TEnum>(IUI ui, string prompt) where TEnum : struct, Enum
+    {
+        var values = Enum.GetValues<TEnum>();
+        var options = string.Join("\n", values.Select((value, i) => $"{i + 1}. {value}"));
+        var index = PromptUntilValidNumber(ui, $"{prompt}\n{options}", (x) => x >= 1 && x <= values.Length);
+
+        return values[index - 1];
+    }
 }
