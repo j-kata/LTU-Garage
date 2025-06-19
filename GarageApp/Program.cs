c00b328164ba7c716e50f47d6038bdc2a09292d5
katja tsareva <katja.tcareva@gmail.com>
2025-06-19 11:37:30 +0200

    add Program

--- GarageApp/Program.cs
+++ GarageApp/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace GarageApp.Application;
+﻿using GarageApp.Handler;
+using GarageApp.Loader;
+using GarageApp.Manager;
+using GarageApp.UI;
+
+namespace GarageApp;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var manager = new GarageManager(
+            new ConsoleUI(),
+            new GarageHandler(),
+            new StaticLoader());
+
+        manager.Run();
     }
 }
